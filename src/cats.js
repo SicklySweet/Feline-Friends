dfe662f21dbf8dbf1c8e6054672b59e31275b173
Christian <us3l3ss1@hotmail.com>
2019-07-10 02:53:24 -0700

    initial commit

---
+++ src/cats.js
@@ -0,0 +1,62 @@
+export const cats = [
+  {
+    id: 1,
+    name: 'Herman Schultz',
+    username: 'Shocker',
+    email: 'shocker@villains.com'
+  },
+  {
+    id: 2,
+    name: 'Scott Summers',
+    username: 'Cyclops',
+    email: 'leader@xmen.com'
+  },
+  {
+    id: 3,
+    name: 'Wade Wilson',
+    username: 'Deadpool',
+    email: 'mercwithamouth@mercenary.com'
+  },
+  {
+    id: 4,
+    name: 'Michael Meowers',
+    username: 'The-Shape',
+    email: 'shape@evilwithin.com'
+  },
+  {
+    id: 5,
+    name: 'Cat Benatar',
+    username: 'Crimes-of-Passion',
+    email: 'mymanager@promotions.com'
+  },
+  {
+    id: 6,
+    name: 'Bubba Sawyer',
+    username: 'Leatherface',
+    email: 'bbq@texas.com'
+  },
+  {
+    id: 7,
+    name: 'Greer Grant',
+    username: 'Tigra',
+    email: 'tigra@westcoastavengers.com'
+  },
+  {
+    id: 8,
+    name: 'Barbara Ann Minerva',
+    username: 'Cheetah',
+    email: 'cheetah@legionofdoom.com'
+  },
+  {
+    id: 9,
+    name: 'Chris Catt',
+    username: 'Starlord',
+    email: 'starlord@guardians.galaxy'
+  },
+  {
+    id: 10,
+    name: 'Steve Mouser',
+    username: 'Captain Americat',
+    email: 'steve@catvengers.com'
+  }
+];
